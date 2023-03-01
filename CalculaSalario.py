cfc2443415476718a99e21e4bca68c8f30646e08
CrysKawaii <cryskawaiii@gmail.com>
2023-03-01 16:22:44 -0300

    Pede a quantidade que ganha por hora e quantas horas trabalha no mês e calcula o salário

---
+++ CalculaSalario.py
@@ -0,0 +1,3 @@
+earn_hour = float(input('Digite quanto você ganha por hora: '))
+hour_month = float(input('Digite quantas horas você trabalha no mês: '))
+print(f'No mês você ganha R${earn_hour * hour_month:.2f}')
