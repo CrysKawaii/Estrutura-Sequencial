ea671e18e44ebad7fea960719ed0a597679aee70
CrysKawaii <cryskawaiii@gmail.com>
2023-03-01 15:49:13 -0300

    Soma 4 notas e mostra a média entre elas

---
+++ MediaBimestral.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite a 1° nota: '))
+n2 = float(input('Digite a 2° nota: '))
+n3 = float(input('Digite a 3° nota: '))
+n4 = float(input('Digite a 4° notas: '))
+average = (n1 + n2 + n3 + n4) / 4
+print(f'A média desse aluno é {average:.1f}')
