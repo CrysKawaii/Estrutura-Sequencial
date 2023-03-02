1ac375d7d4b8c1b2b74c79ff9bf4dde6657ebf23
CrysKawaii <cryskawaiii@gmail.com>
2023-03-02 08:44:32 -0300

    Pede a altura e calcula o peso ideal para homens e mulheres

---
+++ IMC2.py
@@ -0,0 +1,5 @@
+height = float(input('Digite sua altura: '))
+ideal_weight_man = (72.7 * height) - 58
+ideal_weight_woman = (62.1 * height) - 44.7
+print(f'Sua altura ideal supondo que seja homem é {ideal_weight_man:.2f}')
+print(f'Supondo que seja mulher é {ideal_weight_woman:.2f}')
